6f2284f30aecbe75ef308581c8a3bd74b895650d
ahmedgarci <ahmedgarci146@gmail.com>
2026-05-31 22:26:08 +0200

    test

--- src/test/java/com/example/messageBroker/MessageBrokerApplicationTests.java
+++ src/test/java/com/example/messageBroker/MessageBrokerApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.messageBroker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MessageBrokerApplicationTests {
 
 	@Test
